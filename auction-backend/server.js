b66264fd84665324cf070d48e293aee6efd93bc2
BEBIE GRACE BALBUENA <BALB20248345@MLHUILLIER.LOCAL>
2024-05-03 16:31:21 +0800

    Add backend files [Bebie]

---
+++ auction-backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+host: "localhost",
+user: 'root',
+password:'',
+database: 'auction_data'
+})
+
+app.get('/', (re, res)=> {
+return res.json("From BAckend Side");
+})
+
+app.get('/users', (req, res)=> {
+const sql = "SELECT * FROM users";
+db.query(sql, (err, data)=> {
+if(err) return res.json(err);
+return res.json(data);
+
+})
+})
+
+app.listen(8081,()=> {
+console.log("listening");
+})
+
+
+
