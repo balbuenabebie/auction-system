f9750d84818ac67ef6d7f9a0834e9f14731e190f
Nicarlou Jamin <JAMI2024022@MLHUILLIER.LOCAL>
2024-05-08 10:47:18 +0800

    save [nicks]

--- auction-frontend/src/client-component/client_header.jsx
+++ auction-frontend/src/client-component/client_header.jsx
@@ -6,7 +6,7 @@ function ClientHeader() {
         <div>
             <nav style={{ backgroundColor: 'white', width: '100%' }}>
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <ul className="text-red-500 flex flex-col font-medium p-4 md:p-0 mt-4  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white  dark:border-gray-700">
+                    <ul className="text-red-500 flex flex-row font-medium p-4 md:p-0 mt-4  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white  dark:border-gray-700">
                         <li>
                             <Link to="/" className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-red md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Auctions</Link>
                         </li>
---
+++ auction-frontend/src/pages/admin/edit_lots.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const edit_lots = () => {
+  return (
+    <div>
+      EDIT LOTS
+    </div>
+  )
+}
+
+export default edit_lots
